dc18bcfb8292967a6c071902abbdd4cf5ed6318c
WINDOWS-4\Administrator <zxf@linhuiba.com>
2019-05-29 22:03:54 +0800

    第16次测试

---
+++ mylibrary/src/main/java/com/linhuiba/mylibrary/TestConfig.java
@@ -0,0 +1,5 @@
+package com.linhuiba.mylibrary;
+
+public class TestConfig {
+    public static final String TEST_LIBRARY = "TEST_LIBRARY";
+}
--- mylibrary1/src/main/java/com/linhuiba/mylibrary1/Config.java
+++
@@ -1,5 +0,0 @@
-package com.linhuiba.mylibrary1;
-
-public class Config {
-    public static final String  TESTCONFIG = "mylibrary";
-}
