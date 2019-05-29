0167c6cfd5fa3c93a1d3e9c963066e51d7e0033a
WINDOWS-4\Administrator <zxf@linhuiba.com>
2019-05-29 16:36:22 +0800

    第4次测试

---
+++ mylibrary/src/main/java/com/linhuiba/mylibrary/Config.java
@@ -0,0 +1,12 @@
+package com.linhuiba.linhuipublic.config;
+
+
+import com.linhuiba.linhui_public.BuildConfig;
+
+/**
+ * Created by snowd on 15/3/27.
+ */
+public class Config {
+    public static final String TEST_CONFIG = "1112";//美洽
+}
+
